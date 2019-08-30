88d6b3134e295789154a7f60bd865ce8e4b64934
ddeveloper72 <duncan.niamh@gmail.com>
2019-08-31 00:33:19 +0100

    Add a method for subscribing to the errorSubscription in ngOnInit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
 import { PostsService } from './posts.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts = [];
   isFetching = false;
   error = null;
+  private errorSubscription: Subscription;
 
   constructor(private http: HttpClient, private postService: PostsService) {}
 
   ngOnInit() {
+    this.errorSubscription = this.postService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    });
     this.isFetching = true;
     this.postService.fetchPosts().subscribe(posts => {
       this.isFetching = false;
@@ -49,4 +54,7 @@ export class AppComponent implements OnInit {
     });
   }
 
+    ngOnDestroy() {
+      this.errorSubscription.unsubscribe();
+    }
 }
