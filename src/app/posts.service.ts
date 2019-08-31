805e426313c5fbfc3b97328026f1c2e2147db938
ddeveloper72 <duncan.niamh@gmail.com>
2019-08-31 11:09:07 +0100

    Demonstrating the use of HttpHeaders in the fetchPosts method.

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
 
 import { Post } from './post.model';
@@ -30,7 +30,10 @@ export class PostsService {
     fetchPosts() {
         // ...
         return this.http
-          .get<{ [key: string]: Post }>('https://angular-http-requests-cb030.firebaseio.com/posts.json')
+          .get<{ [key: string]: Post }>('https://angular-http-requests-cb030.firebaseio.com/posts.json',
+          {
+            headers: new HttpHeaders({'Custom-Header': 'Hello-World'})
+          })
           .pipe(map(responseData => {
               const postsArray: Post[] = [];
               for (const key in responseData) {
