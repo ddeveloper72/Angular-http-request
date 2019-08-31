c69e7a323624e7be2c8d001766774623e6bfed04
ddeveloper72 <duncan.niamh@gmail.com>
2019-08-31 15:15:33 +0100

    Removed pipe element now being handled by logging interceptor.

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,23 +1,12 @@
 import { HttpInterceptor, 
    HttpRequest, 
    HttpHandler ,
-   HttpEventType
 } from '@angular/common/http';
-import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
- intercept(req: HttpRequest<any>, next: HttpHandler) {
-    console.log('Request is on the way');
-    console.log(req.url);
+ intercept(req: HttpRequest<any>, next: HttpHandler) {    
     const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')
    });
-    return next.handle(modifiedRequest).pipe(
-       tap(event => {
-       if (event.type === HttpEventType.Response) {
-          console.log('Response: The body data has arrived');
-          console.log(event.body);
-       }
-    })
-   );
+    return next.handle(modifiedRequest);
  }
 }
