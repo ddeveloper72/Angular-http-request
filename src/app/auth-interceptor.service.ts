48786affc88ed83fe949ea610e6273ee98716a24
ddeveloper72 <duncan.niamh@gmail.com>
2019-08-31 12:15:09 +0100

    Corrected a ; error on (5,41)

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -2,7 +2,7 @@ import { HttpInterceptor, HttpRequest, HttpHandler } from "@angular/common/http"
 
 export class AuthInterceptorService implements HttpInterceptor {
  intercept(req: HttpRequest<any>, next: HttpHandler) {
-    console.log('Request is on the way'):
+    console.log('Request is on the way');
     return next.handle(req);
  }
 }
